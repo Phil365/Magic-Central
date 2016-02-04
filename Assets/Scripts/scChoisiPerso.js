6d82128f617b8bd1f4344076475ed432c20a878e
EcchiHiro <alexbouet.ca@gmail.com>
2016-02-04 01:23:38 -0500

    Ajout lvl 5 / Correction Tir / Selection des personnages

--- Assets/Scripts/scChoisiPerso.js
+++ Assets/Scripts/scChoisiPerso.js
@@ -4,7 +4,7 @@ public var hero1: GameObject;
 public var hero2: GameObject;
 public var hero3: GameObject;
 
-var heroChoisi : int = 1; // par défaut le héros 1 est sélectionner si utilisateur ne sélectionne personne 
+private var heroChoisi : int = 1; // par défaut le héros 1 est sélectionner si utilisateur ne sélectionne personne 
 function Start () {
 hero1.SetActive(true);
 }
@@ -18,7 +18,7 @@ function Update () {
 function ChoisiHero1(){ //choisi le premier héro et désactive les autres
 //	Debug.Log('Hero 1');
 	heroChoisi = 1;
-	PlayerPrefs.SetInt('heroChoisi',(heroChoisi)); // enregistre le héros dans les préférences pour récupérer dans les autres scenes
+	PlayerPrefs.SetInt('heroChoisi',heroChoisi); // enregistre le héros dans les préférences pour récupérer dans les autres scenes
 	hero1.SetActive(true);
 	hero2.SetActive(false);
 	hero3.SetActive(false);
@@ -27,7 +27,7 @@ function ChoisiHero1(){ //choisi le premier héro et désactive les autres
 function ChoisiHero2(){
 //	Debug.Log('Hero 2');
 	heroChoisi = 2;
-	PlayerPrefs.SetInt('heroChoisi',(heroChoisi));
+	PlayerPrefs.SetInt('heroChoisi',heroChoisi);
 	hero1.SetActive(false);
 	hero2.SetActive(true);
 	hero3.SetActive(false);
@@ -36,7 +36,7 @@ function ChoisiHero2(){
 function ChoisiHero3(){
 //	Debug.Log('Hero 3');
 	heroChoisi = 3;
-	PlayerPrefs.SetInt('heroChoisi',(heroChoisi));
+	PlayerPrefs.SetInt('heroChoisi',heroChoisi);
 	hero1.SetActive(false);
 	hero2.SetActive(false);
 	hero3.SetActive(true);
